e8e58b485cc12e881f90d37442418c1de2162dbd
GordonSmith <GordonSmith@users.noreply.github.com>
2022-01-18 08:16:24 +0000

    deploy: b4a03b3c9ec67dce5057bb78a8fbcae2a04db3f1

---
+++ assets/src_codemirror_codemirror.md.bcac1666.js
@@ -0,0 +1,14 @@
+import{_ as r,c as d,e as i,w as a,b as e,d as t,a as n,r as c,o as s}from"./app.db3d4f43.js";const w='{"title":"CodeMirror Editor","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"mode","slug":"mode"},{"level":3,"title":"text","slug":"text"},{"level":3,"title":"theme","slug":"theme"},{"level":2,"title":"Events","slug":"events"}],"relativePath":"src/codemirror/codemirror.md","lastUpdated":1642493652522}',l={},h=e("h1",{id:"codemirror-editor",tabindex:"-1"},[t("CodeMirror Editor "),e("a",{class:"header-anchor",href:"#codemirror-editor","aria-hidden":"true"},"#")],-1),p=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-codemirror>")],-1),u=e("hpcc-preview",{preview_border:"0px",preview_height_ratio:"0.5",style:{width:"100%",height:"400px"}},[e("pre",null,[e("code",null,`  <hpcc-codemirror mode="json" theme="dark" style="width:100%;height:100%">
+  </hpcc-codemirror>
+  <script>
+    customElements.whenDefined("hpcc-codemirror").then(() => {
+      document.querySelector('hpcc-codemirror').text = \`\\
+{
+  "aaa":123, 
+  "bbb":"ddd", 
+  "c":3, 
+  "d":true
+}\`;
+    });
+  <\/script>
+`)])],-1),m=n('<div class="tip custom-block"><p class="custom-block-title">TIP</p><p>See <a href="./../../README.html">Getting Started</a> for details on how to include @hpcc-js/web-components in your application</p></div><h2 id="attributes" tabindex="-1">Attributes <a class="header-anchor" href="#attributes" aria-hidden="true">#</a></h2><h3 id="mode" tabindex="-1"><code>mode</code> <a class="header-anchor" href="#mode" aria-hidden="true">#</a></h3><p><i>Text to be displayed in the editor</i></p><p><strong>Type:</strong> <code>&quot;html&quot; | &quot;json&quot;</code></p><ul><li><p><code>html</code>: HTML document</p></li><li><p><code>JSON</code>: JSON document</p></li></ul><p><strong>Default Value:</strong> html</p><h3 id="text" tabindex="-1"><code>text</code> <a class="header-anchor" href="#text" aria-hidden="true">#</a></h3><p><i>Text to be displayed in the editor</i></p><p><strong>Type:</strong> <code>string</code></p><p><strong>Default Value:</strong></p><h3 id="theme" tabindex="-1"><code>theme</code> <a class="header-anchor" href="#theme" aria-hidden="true">#</a></h3><p><i>Text to be displayed in the editor</i></p><p><strong>Type:</strong> <code>&quot;light&quot; | &quot;dark&quot;</code></p><ul><li><p><code>light</code>: Light theme</p></li><li><p><code>dark</code>: Dark theme</p></li></ul><p><strong>Default Value:</strong> light</p><h2 id="events" tabindex="-1">Events <a class="header-anchor" href="#events" aria-hidden="true">#</a></h2>',17);function _(g,b,x,f,v,T){const o=c("ClientOnly");return s(),d("div",null,[h,p,i(o,null,{default:a(()=>[u]),_:1}),m])}var q=r(l,[["render",_]]);export{w as __pageData,q as default};
---
+++ assets/src_gauge_gauge.md.3c9ae648.lean.js
@@ -0,0 +1,19 @@
+import{_ as a,c as i,e as r,w as l,b as e,d as t,a as n,r as c,o as s}from"./app.db3d4f43.js";const w='{"title":"Gauge","description":"","frontmatter":{},"headers":[{"level":2,"title":"Attributes","slug":"attributes"},{"level":3,"title":"color_domain","slug":"color-domain"},{"level":3,"title":"color_range","slug":"color-range"},{"level":3,"title":"empty_color","slug":"empty-color"},{"level":3,"title":"show_tick","slug":"show-tick"},{"level":3,"title":"tick_color","slug":"tick-color"},{"level":3,"title":"tick_value_description","slug":"tick-value-description"},{"level":3,"title":"tick_value","slug":"tick-value"},{"level":3,"title":"title_description","slug":"title-description"},{"level":3,"title":"title","slug":"title"},{"level":3,"title":"tooltip","slug":"tooltip"},{"level":3,"title":"value_description","slug":"value-description"},{"level":3,"title":"value","slug":"value"},{"level":2,"title":"Events","slug":"events"}],"relativePath":"src/gauge/gauge.md","lastUpdated":1642493652522}',d={},p=e("h1",{id:"gauge",tabindex:"-1"},[t("Gauge "),e("a",{class:"header-anchor",href:"#gauge","aria-hidden":"true"},"#")],-1),u=e("p",null,[e("strong",null,"tag"),t(": "),e("code",null,"<hpcc-gauge>")],-1),h=e("hpcc-preview",{preview_border:"0px",preview_height_ratio:"0.33",style:{width:"100%",height:"400px"}},[e("pre",null,[e("code",null,`<hpcc-splitpanel style="width:100%;height:100%">
+  <hpcc-gauge show_tick title="G-01" title_description="Gauge 01" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+  <hpcc-gauge title="G-02" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+  <hpcc-gauge show_tick title="G-03" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+  <hpcc-gauge title="G-04" style="width:100%;min-width:48px;height:100%">
+  </hpcc-gauge>
+</hpcc-splitpanel>
+<script>
+    customElements.whenDefined("hpcc-splitpanel").then(() => {
+      for (const gauge of document.querySelectorAll("hpcc-gauge")) {
+          gauge.value = Math.random();
+          gauge.tick_value = Math.random();
+      }
+    });
+<\/script>
+`)])],-1),g=n("",51);function v(_,f,m,q,y,b){const o=c("ClientOnly");return s(),i("div",null,[p,u,r(o,null,{default:l(()=>[h]),_:1}),g])}var x=a(d,[["render",v]]);export{w as __pageData,x as default};
